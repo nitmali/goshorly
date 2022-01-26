44e7b3066c58265c1435bcc50ee49f1ca0866b00
Phil <phil@ucode.space>
2022-01-26 21:01:57 +0100

    Added limiter json

--- utils/limiter.go
+++ utils/limiter.go
@@ -11,6 +11,12 @@ var ConfigLimiter limiter.Config = limiter.Config{
 	Max:        10,
 	Expiration: 60 * time.Second,
 	LimitReached: func(c *fiber.Ctx) error {
+		if c.Get("content-type") == "application/json" {
+			return c.Status(418).JSON(fiber.Map{
+				"msg":     "Too many requests, slow down I'm a teapot (10 requests per minute)",
+				"success": false,
+			})
+		}
 		return c.Render("views/home", fiber.Map{
 			"ERR":                 "You have reached the limit of requests! Please check back later. (1 minute)",
 			"CI_COMMIT_SHORT_SHA": CI_COMMIT_SHORT_SHA,
